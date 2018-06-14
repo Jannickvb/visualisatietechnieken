7e95613cf023c417f46c47648309b8b5a302f939
Jannick van Ballegooijen <jannickvb@msn.com>
2018-06-14 13:51:53 +0200

    fix clear post processing

--- postprocess.fs
+++ postprocess.fs
@@ -8,15 +8,7 @@ void main()
 {
     float o = time/2048.0;
 
-/*    vec4 color = (5 * texture2D(s_texture, texCoord) -
-                texture2D(s_texture, texCoord + vec2(-o, 0)) -
-                texture2D(s_texture, texCoord + vec2( o, 0)) -
-                texture2D(s_texture, texCoord + vec2(0, o)) -
-                texture2D(s_texture, texCoord + vec2(0, -o)));
-				*/
-
-	vec4 color = texture2D(s_texture, texCoord + 0.01 * vec2(sin(time + texCoord.y * 10), sin(time*0.5 + texCoord.x * 12)));
-
+	vec4 color = texture2D(s_texture, texCoord + 0.01 * vec2(sin(time + texCoord.y * 20), sin(time*0.5 + texCoord.x * 24)));
 
 	gl_FragColor = color;
 }
